f2600e4b105ae106b229174d5cf81f621ae9b8c4
MdSaayed <sayedkhan1786.com>
2024-04-05 16:47:26 +0600

    Update origin

--- src/components/WeServe.jsx
+++ src/components/WeServe.jsx
@@ -2,7 +2,7 @@
 const WeServe = () => {
   return (
     <section>
-      <div className="max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8 lg:py-16">
+      <div className="max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8 lg:py-16 container">
         <div
           className="grid grid-cols-1 gap-y-8 lg:grid-cols-2 lg:items-center lg:gap-x-16"
         >
